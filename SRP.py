164bcdb82f4037e0a115091722b7db26a12e0b8e
mendy hershkop <hers.mm@gmail.com>
2025-11-05 09:41:46 +0200

    realization of RSP

--- SRP.py
+++ SRP.py
@@ -1,3 +1,4 @@
+#1
 class Book:
     def __init__(self,title:str, author:str, content:str):
         self.title=title
@@ -8,7 +9,36 @@ class Book:
     #         file.write(self.content)
     #  wrong exemple
 
-class Write(Book):
-    def save_to_file(self,filname):
+class Write:
+    @staticmethod
+    def save_to_file(filname,content):
         with open(filname, 'w') as file:
-            file.write(self.content)
+            file.write(content)
+
+#2
+class Student:
+    def __init__(self,name:str,grades:list[int]):
+        self.name=name
+        self.grades=grades
+    # def average_grade(self):
+    #     return sum(self.grades)/len(self.grades)
+    # wrong exemple
+class GradeCalculator:
+    @staticmethod
+    def average_grade(grades):
+        return sum(grades)/len(grades)
+
+#3
+class Order:
+    def __init__(self,items:list[str],total_price:float):
+        self.items=items
+        self.total_price=total_price
+    # def print_invoice(self):
+    #     print(sorted(self.items))
+    #     print(self.total_price)
+    #  wrong exemple
+
+class InvoicePrinter:
+    @staticmethod
+    def print_invoice(item):
+        print(sorted(item))
